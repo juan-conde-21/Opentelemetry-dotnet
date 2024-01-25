9450e9b80c6e26c44157e1cf7be26763bfdfb724
Juan Conde <juan.conde@ibm.com>
2024-01-25 19:27:29 +0000

    Agregado Dockerfile

---
+++ app/Program.cs
@@ -0,0 +1,47 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Diagnostics;
+#if NETFRAMEWORK
+using System.Net.Http;
+#endif
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+//using OpenTelemetry.Exporter.Instana;
+
+namespace GettingStartedJaeger;
+
+public class Program
+{
+    private static readonly ActivitySource MyActivitySource = new("OpenTelemetry.Demo.Jaeger");
+
+    public static async Task Main()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
+                serviceName: "CesarDemoApp",
+                serviceVersion: "1.0.0"))
+            .AddSource("OpenTelemetry.Demo.Jaeger")
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter()
+            .Build();
+
+        using var parent = MyActivitySource.StartActivity("JaegerDemo");
+
+        using (var client = new HttpClient())
+        {
+            using (var slow = MyActivitySource.StartActivity("SomethingSlow"))
+            {
+                await client.GetStringAsync("https://httpstat.us/200?sleep=1000");
+                await client.GetStringAsync("https://httpstat.us/200?sleep=1000");
+            }
+
+            using (var fast = MyActivitySource.StartActivity("SomethingFast"))
+            {
+                await client.GetStringAsync("https://httpstat.us/301");
+            }
+        }
+    }
+}
